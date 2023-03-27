7afa0fde9e3888220e4fa91df0b593cf0c9520d0
joselatines <joselatines33@gmail.com>
2023-03-26 20:18:02 -0400

    Add email functionality and order details to ShippingForm submission

--- src/components/ShippingForm/index.tsx
+++ src/components/ShippingForm/index.tsx
@@ -2,91 +2,140 @@ import { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import {
+	FormControl,
+	FormLabel,
+	Input,
+	Button,
+	Select,
+} from '@chakra-ui/react';
 import { useCustomToast } from '@/lib/hooks/useCustomToast';
 import { useRouter } from 'next/router';
+import { useCart } from '@/lib/context/CartContext';
 
+const initialValues = {
+	fullName: '',
+	city: '',
+	parish: '',
+	phoneNumber: '',
+	email: '',
+	paymentMethod: '',
+};
+
+const validationSchema = Yup.object({
+	fullName: Yup.string().required('Required'),
+	city: Yup.string().required('Required'),
+	parish: Yup.string().required('Required'),
+	phoneNumber: Yup.string()
+		.matches(/^[0-9]+$/, 'Must be only digits')
+		.min(10, 'Must be exactly 10 digits')
+		.required('Phone number is required'),
+	email: Yup.string().email('Invalid email address').required('Required'),
+	paymentMethod: Yup.string(),
+});
+
+type formValues = {
+	fullName: string;
+	city: string;
+	parish: string;
+	phoneNumber: string;
+	email: string;
+	paymentMethod: string;
+};
 
 export default function ShippingForm() {
 	const showToast = useCustomToast();
 	const router = useRouter();
 	const [payment, setPayment] = useState('efectivo');
+	const { getProducts } = useCart();
 
-	type formValues = {
-		fullName: string;
-		city: string;
-		parish: string;
-		phoneNumber: string;
-		email: string;
-		paymentMethod: string;
-	};
-
-	const sendEmail = (
-		userFullName: string,
-		userEmail: string,
-		message: string
-	) => {
+	const sendEmail = async (userEmail: string, message: string) => {
 		const templateParams = {
-			from_name: userFullName,
+			from_name: userEmail,
 			reply_to: userEmail,
-			message_html: message,
+			to_name: 'joselatines33@gmail.com',
+			message,
 		};
 
-		emailjs
-			.send(
-				'service_8gpefgi',
-				'template_zmcgusl',
+		const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE;
+		const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE;
+		const EMAILJS_USER_ID = process.env.NEXT_PUBLIC_EMAILJS_USER_ID;
+
+		try {
+			if (!SERVICE_ID || !TEMPLATE_ID || !EMAILJS_USER_ID) {
+				throw new Error('Credentials are not available');
+			}
+
+			const response = await emailjs.send(
+				SERVICE_ID,
+				TEMPLATE_ID,
 				templateParams,
-				'jehC29-_LeK6wUf5s9ge7'
-			)
-			.then(
-				result => {
-					console.log(result.text);
-				},
-				error => {
-					console.log(error.text);
-				}
+				EMAILJS_USER_ID
 			);
-	};
-	const createOrder = async (order: any) => {
-		await sendEmail(order.fullName, order.email, 'holi bb');
-	};
 
-	const initialValues = {
-		fullName: '',
-		city: '',
-		parish: '',
-		phoneNumber: '',
-		email: '',
-		paymentMethod: '',
-	};
+			return response;
+		} catch (error) {
+			console.error(error);
 
-	const validationSchema = Yup.object({
-		fullName: Yup.string().required('Required'),
-		city: Yup.string().required('Required'),
-		parish: Yup.string().required('Required'),
-		phoneNumber: Yup.string()
-			.matches(/^[0-9]+$/, 'Must be only digits')
-			.min(10, 'Must be exactly 10 digits')
-			.required('Phone number is required'),
-		email: Yup.string().email('Invalid email address').required('Required'),
-		paymentMethod: Yup.string(),
-	});
+			showToast({
+				title: error.message,
+				status: 'error',
+				duration: 5000,
+				isClosable: true,
+			});
+
+			return error;
+		}
+	};
 
 	const formik = useFormik({
 		initialValues,
-		validationSchema: validationSchema,
+		validationSchema,
 		onSubmit: async values => {
-			// Handle form submission
-			await createOrder({ ...values, paymentMethod: payment });
+			formik.setSubmitting(true);
+			const order = { ...values, paymentMethod: payment };
+			const products = await getProducts();
+			
+			const templateMessage = `
+			Detalles del pedido
+			Nombre: ${order.fullName}
+			Ciudad: ${order.city}
+			Parroquia: ${order.parish}
+			Método de pago: ${order.paymentMethod}
+
+			Productos: 
+			${products
+				.map(
+					product => `
+			Nombre: ${product.title}
+			Cantidad: ${product.qty}
+			Precio: ${product.price}
+		`
+				)
+				.join('')}
+`;
+
+			const res = await sendEmail(order.email, templateMessage);
+
+			if (res.status !== 200) {
+				showToast({
+					title: 'No se pudo enviar la orden',
+					status: 'error',
+					duration: 5000,
+					isClosable: true,
+				});
+				return;
+			}
 
 			showToast({
-				title: 'Order Submitted!',
+				title: 'Orden enviada con éxito! Revisa tu correo',
 				status: 'success',
 				duration: 5000,
 				isClosable: true,
 			});
-			// router.push('/');
+			router.push('/');
+
+			formik.setSubmitting(false);
 		},
 	});
 
@@ -179,10 +228,7 @@ export default function ShippingForm() {
 				</select>
 			</FormControl>
 
-			<Button
-				disabled={!formik.isValid || !formik.dirty || !formik.isSubmitting}
-				type='submit'
-			>
+			<Button disabled={formik.isSubmitting} type='submit'>
 				Submit
 			</Button>
 		</form>
