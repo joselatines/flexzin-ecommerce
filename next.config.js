2d1636d3c5f914a9c636102077c20d4d221694cd
joselatines <joselatines33@gmail.com>
2023-05-23 14:46:44 -0400

    f

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://flexzin-ecommerce.vercel.app/:path*'
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
